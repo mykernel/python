7a470011e7eae5e6a7431284d9b4eaef64129eef
mykernel <2580259468@qq.com>
2019-06-17 14:35:28 +0800

    日志分析v1版

--- 日志分析/log_v1.py
+++ 日志分析/log_v1.py
@@ -1,45 +1,34 @@
-# 分解日志
-
-import datetime
+# 读取文件中所有的文件
+from pathlib import Path
+from itertools import islice
 import re
 
-logline = '''110.184.45.122 - - [28/Apr/2019:00:04:01 +0800] "GET /v1/images/points/sign_bg.png HTTP/1.1" 304 - "http://wx.kuaijiankang.com/v1/deals" "Mozilla/5.0 (iPhone; CPU iPhone OS 12_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 MicroMessenger/7.0.3(0x17000321) NetType/WIFI Language/zh_CN"
-'''
+path_list = []
 
-ops1 = {
-    'datetime': lambda timestr: datetime.datetime.strptime(timestr, '%d/%b/%Y:%H:%M:%S %z'),
-    'status': int,
-    'size': int
-}
-ops2 = {
-    'datetime': lambda timestr: datetime.datetime.strptime(timestr, '%d/%b/%Y:%H:%M:%S %z'),
-    'status': int
-}
+p1 = Path("C:\\Users\\Administrator\\Desktop\\log")
 
-pattern = '''(?P<remote>[\\d.]{7,}) - - \\[(?P<datetime>.+)\\] "(?P<method>\\w+) (?P<get_url>[\\S/]+) \
-(?P<protocol>[\\S/]+)" (?P<status>\\d+) (?P<size>[\\d-]+) "(?P<url>.*)" "(?P<useragent>.+)
-'''
-regex = re.compile(pattern)
+pys = p1.glob('*.log')
 
+for i in pys:
+    path_list.append(i)
 
-def extract(line) -> dict:
-    """正常的返回应为字典，如果返回None说明匹配失败"""
-    matcher = regex.match(line)
-    print(line)
+for j in path_list:
+    print(str(j))
+    with open(str(j), encoding='UTF-8') as f:
+        while True:
+            next_n_lines = list(islice(f, 6))
+            str_lines = str(next_n_lines)
+            # print(next_n_lines)
+            if len(next_n_lines) == 6 and next_n_lines != []:
 
-    info = None
-    if matcher:
-        if '" 304' in line:
-            info = {k: ops2.get(k, lambda x: x)(v) for k, v in matcher.groupdict().items()}
-        else:
-            info = {k: ops1.get(k, lambda x: x)(v) for k, v in matcher.groupdict().items()}
-    return info
+                # print(next_n_lines[5])
 
+                pattern = r'''(?P<date>[\w\s:-]+)\S+\w+\S\s+(?P<time>[\d.]+)'''
 
-print(extract(logline))
+                regex = re.compile(pattern)
 
-# 知识点
-# 1. 正则表达式
-# 2. 字典的v的格式化
+                matcher = regex.findall(next_n_lines[5])
+                print(matcher)
 
-#nn
+            if not next_n_lines:
+                break
